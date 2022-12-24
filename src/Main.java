83d26507e499d2c80fa9340fe86cc81ae9196940
МАРК <markpan07@mail.ru>
2022-12-24 22:19:22 +0300

    Homework is done!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        // Комментарий для создания новой ветки
         task1();
         System.out.println();
         task2();
